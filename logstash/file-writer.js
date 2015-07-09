100c4c717f598bd4f54a4f070d84ea07df3900aa
unknown <somnath.panja@BLR1-LDL-33298.synapse.com>
2015-07-09 16:49:00 +0530

    Sample log file writter

---
+++ logstash/file-writer.js
@@ -0,0 +1,39 @@
+/**
+ * Created by somnath.panja on 7/9/2015.
+ */
+
+(function () {
+    var fs = require('fs');
+    var FileWritter = function (path, fileName) {
+        this._fileName = path + "/" + fileName;
+    };
+
+    FileWritter.prototype.write = function (data, cb) {
+        var data2Write = data;
+        if (typeof data == "object") {
+
+            try {
+                data2Write = JSON.stringify(data);
+            } catch (e) {
+                data2Write = e.toString();
+            }
+
+        }
+
+        data2Write += '\n';
+
+        if (cb) {
+            fs.appendFile(this._fileName, data2Write, function (err) {
+                if (err) throw err;
+                cb.apply(null, [err]);
+            });
+        } else {
+            fs.appendFile(this._fileName, data2Write, function (err) {
+                console.log(err);
+            });
+        }
+    };
+
+    module.exports = FileWritter;
+
+})();
---
+++ logstash/index.js
@@ -0,0 +1,83 @@
+/**
+ * Created by somnath.panja on 7/9/2015.
+ */
+/**
+ * Generate 3 log files randomly
+ */
+
+
+(function(){
+    var longjohn = require('longjohn');
+    longjohn.async_trace_limit = 3;
+    process.name = "myapp";
+    var fileWritter = require('./file-writer');
+    var debugLogger = new fileWritter('./logs', process.name + "_debug.log");
+
+    var sampleErrorMsgs = [
+        "Memory exception",
+        "Max socket reached",
+        "failed to write to file",
+        "cant access property of undefined",
+        "Failed to publish amqp",
+        "Failed to connect to mongodb"
+    ];
+
+    var getRandomNumber = function(start, end){
+        return Math.floor((Math.random() * end) + start);
+    };
+
+    function write2DebugLog(message, error) {
+        var sampleData = {
+            message: message,
+            eror_code: error.code,
+            err: error,
+            time_ms : {
+                "session" : getRandomNumber(1, 100),
+                "parsing" : getRandomNumber(1, 100),
+                "amqp_publish" : getRandomNumber(1, 100),
+            },
+            ts: Date.now(),
+            ts_display: Date.now().toLocaleString(),
+            status: "failed"
+        };
+        console.log(JSON.stringify(sampleData));
+        debugLogger.write(sampleData);
+    }
+
+    var taskA = function(cb){
+        process.nextTick(function(){
+            console.log('Im doing task A');
+            taskB(function(){
+                cb.apply(null, [null]);
+            });
+        });
+    };
+
+    var taskB = function(cb){
+        process.nextTick(function(){
+            console.log('Im doing task B');
+            taskC(function(){
+                cb.apply(null, [null]);
+            });
+        });
+    };
+
+    var taskC = function(cb){
+        process.nextTick(function(){
+            console.log('Im doing task C');
+            throw new Error(sampleErrorMsgs[getRandomNumber(0, 5)]);
+            cb.apply();
+        });
+    };
+
+    (function sampleAppCode2Run(){
+        setTimeout(function(){
+            taskA();
+            setTimeout(sampleAppCode2Run, getRandomNumber(100, 5000));
+        }, 1000);
+    })();
+
+    process.on('uncaughtException', function(err) {
+        write2DebugLog(err.toString(), err);
+    });
+})();
