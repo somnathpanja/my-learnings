a7c4ca01f210aa32088b5fe3443b1f5fba6a6561
unknown <somnath.panja@BLR1-LDL-33298.synapse.com>
2015-07-13 16:28:18 +0530

    kibana loader

--- kinana3.1.2Server/index.js
+++ kinana3.1.2Server/index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express();
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(80, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
